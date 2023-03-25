22a27f9dabc9817588c6bb44b8a7b991b060410d
SemihB <semihr5171@gmail.com>
2023-03-25 18:01:46 -0400

    Home sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LeftSideBar from "../../components/LeftSidebar/LeftSidebar";
 
 const Home = () => {
-    return <div>Home</div>;
+    return <div className="grid grid-cols-1 md:grid-cols-4">
+        <div>
+            <LeftSideBar />
+        </div>
+    </div>;
 };
 
 export default Home;
