79674bd4253510cdb4e7d32734e7a09c797dc6fe
SemihB <semihr5171@gmail.com>
2023-03-25 18:55:27 -0400

    tweet textarea

--- src/components/MainTweet/MainTweet.jsx
+++ src/components/MainTweet/MainTweet.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const MainTweet = () => {
-    return <div>MainTweet</div>
+    return (
+        <div>
+            <p className="font-bold pl-2 my-2">Username</p>
+            <form className="border-b-2 pb-6">
+                <textarea 
+                    type="text" 
+                    placeholder="What's happening" 
+                    maxlength={280}
+                    className="bg-slate-200 rounded-lg w-full p-2"
+                ></textarea>
+                <button className="bg-blue-500 text-white py-2 px-4 rounded-full ml-auto">Tweet</button>
+            </form>
+        </div>
+    )
 };
 
 export default MainTweet;
