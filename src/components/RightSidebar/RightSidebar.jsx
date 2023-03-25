2c1e15e86562525726072c27adafdb1fb0d0f97e
SemihB <semihr5171@gmail.com>
2023-03-25 18:38:43 -0400

    Rightsidebar

---
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RightSidebar = () => {
+    return (
+        <div className="p-6 bg-slate-100 rounded-lg mx-4 space-y-4">
+            <h2 className="font-medium">Trending</h2>
+            <p className="font-bold">#ElonMusk</p>
+            <p className="font-bold">#Rihanna</p>
+            <p className="font-bold">#Beyonce</p>
+            <p className="font-bold">#CocaineBear</p>
+        </div>
+    )
+};
+
+export default RightSidebar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import LeftSideBar from "../../components/LeftSidebar/LeftSidebar";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 
 const Home = () => {
-    return <div className="grid grid-cols-1 md:grid-cols-4">
-        <div>
-            <LeftSideBar />
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4">
+            <div className="px-6">
+                <LeftSidebar />
+            </div>
+            <div className="col-span-2 border-x-2 border-t-slate-800 px-6">
+                <p>Main tweet goes here</p>
+            </div>
+            <div className="px-6">
+                <RightSidebar />
+            </div>
         </div>
-    </div>;
-};
+    );
+}    
 
 export default Home;
