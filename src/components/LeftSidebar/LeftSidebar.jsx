8e5123955d88b0c47ae3c99480369a820d2c5a45
SemihB <semihr5171@gmail.com>
2023-03-29 11:07:24 -0400

    profile options

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -5,10 +5,11 @@ import HomeIcon from "@mui/icons-material/Home";
 import TagIcon from "@mui/icons-material/Tag";
 import PersonIcon from "@mui/icons-material/Person";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/userSlice";
 
 const LeftSidebar = () => {
+    const { currentUser } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 
 	const handleLogout = () => {
@@ -29,7 +30,7 @@ const LeftSidebar = () => {
 						<p>Explore</p>
 					</div>
 				</Link>
-				<Link to="/profile/">
+				<Link to={`/profile/${currentUser._id}`}>
 					<div className="flex items-center space-x-6 px-2 py-2 hover:bg-slate-200 rounded-full cursor-pointer">
 						<PersonIcon fontSize="large" />
 						<p>Profile</p>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
 import RightSidebar from "../../components/RightSidebar/RightSidebar";
 
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
 const Profile = () => {
+	const { currentUser } = useSelector((state) => state.user);
+	const [userTweets, setUserTweets] = useState(null);
+	const [userProfile, setUserProfile] = useState(null);
+
+	const { id } = useParams();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const userTweets = await axios.get(`/tweets/user/all/${id}`);
+				const userProfile = await axios.get(`/users/find/${id}`);
+
+				setUserTweets(userTweets);
+				setUserProfile(userProfile);
+			} catch (err) {
+				console.log("error", err);
+			}
+		};
+		fetchData();
+	}, [currentUser, id]);
+
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-4">
 			<div className="px-6">
 				<LeftSidebar />
 			</div>
 			<div className="col-span-2 border-x-2 border-t-slate-800 px-6" />
+			{/* <img src="" alt="" /> */}
+			{currentUser._id === id ? (
+				<button className="px-4 -y-2 bg-blue-500 rounded-full text-white">
+					Edit Profile
+				</button>
+			) : currentUser.following.includes(id) ? (
+				<button className="px-4 -y-2 bg-blue-500 rounded-full text-white">
+					Following
+				</button>
+			) : (
+				<button className="px-4 -y-2 bg-blue-500 rounded-full text-white">
+					Follow
+				</button>
+			)}
 
 			<div className="px-6">
 				<RightSidebar />
