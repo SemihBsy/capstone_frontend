a0b5681c23eafaf024f0661c0c3d0db329444484
SemihB <semihr5171@gmail.com>
2023-03-29 17:07:43 -0400

    firebase upload

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,9 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import {
+	getStorage,
+	ref,
+	uploadBytesResumable,
+	getDownloadURL,
+} from "firebase/storage";
+
+import app from "../../firebase";
 
 const EditProfile = ({ setOpen }) => {
-    const [img, setImg] = useState(null);
-    const [imgUploadProgress, setImgUploadProgress] = useState();
+	const [img, setImg] = useState(null);
+	const [imgUploadProgress, setImgUploadProgress] = useState();
 
+	const uploadImg = (file) => {
+		const storage = getStorage(app);
+		const fileName = new Date().getTime() + file.name;
+		const storageRef = ref(storage, fileName);
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		// Listen for state changes, errors, and completion of the upload.
+		uploadTask.on(
+			"state_changed",
+			(snapshot) => {
+				// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				setImgUploadProgress(Math.round(progress));
+				switch (snapshot.state) {
+					case "paused":
+						console.log("Upload is paused");
+						break;
+					case "running":
+						console.log("Upload is running");
+						break;
+					default:
+						break;
+				}
+			},
+			(error) => {},
+			() => {
+				// Upload completed successfully, now we can get the download URL
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					console.log("File available at", downloadURL);
+				});
+			},
+		);
+	};
+
+    useEffect(() => {
+        img && uploadImg(img);
+    }, [img]);
 	return (
 		<div className="absolute w-full h-full top-0 left-0 bg-transparent flex items-center justify-center">
 			<div className="w-[600px] h-[600px] bg-slate-200 rounded-lg p-8 flex flex-col gap-4 relative">
@@ -15,17 +62,21 @@ const EditProfile = ({ setOpen }) => {
 				</button>
 				<h2 className="font-bold text-xl">Edit Profile</h2>
 				<p>Choose a new profile picture</p>
+				{imgUploadProgress > 0 ? (
+					"Uploading..." + imgUploadProgress + "%"
+				) : (
+					<input
+						type="file"
+						className="bg-transparent border border-slate-500 rounded p-2"
+						accept="image/*"
+						onChange={(e) => setImg(e.target.files[0])}
+					/>
+				)}
 
-                <input 
-                type="file" 
-                className="bg-transparent border border-slate-500 rounded p-2"
-                accept="image/*"
-                onChange={(e) => setImg(e.target.files[0])}
-                />
-                <p>Delete Acount</p>
-			<button className="bg-red-500 text-white py-2 rounded-full">
-				Delete account
-			</button>
+				<p>Delete Acount</p>
+				<button className="bg-red-500 text-white py-2 rounded-full">
+					Delete account
+				</button>
 			</div>
 		</div>
 	);
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcHpnXf9lAHT31D6aJI48lLya6FBsKFrw",
+  authDomain: "zwitter-4fe03.firebaseapp.com",
+  projectId: "zwitter-4fe03",
+  storageBucket: "zwitter-4fe03.appspot.com",
+  messagingSenderId: "62883688498",
+  appId: "1:62883688498:web:9d3070919feabe349d4c2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
