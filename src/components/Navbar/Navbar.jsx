f0034d5ca7688b5abc579a43af37ffbb31ff459c
SemihB <semihr5171@gmail.com>
2023-03-30 22:05:23 -0400

    profil display fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,12 @@ import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
 import SearchIcon from "@mui/icons-material/Search";
 
 import { useLocation } from "react-router-dom";
+import UserPlaceholder from "../UserPlaceholder/UserPlaceholder";
 
 const Navbar = () => {
 	const [userData, setUserData] = useState(null);
-    const location = useLocation().pathname;
-	
+	const location = useLocation().pathname;
+
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-4 my-5 justify-center">
 			<div className="mx-auto md:mx-0">
@@ -21,10 +22,13 @@ const Navbar = () => {
 
 			<div className="col-span-2 md:border-x-2 md:border-slate-200 md:px-6 my-6 md:my-0">
 				<div className="flex justify-between items-center">
-					
-					<h2 className="font-bold text-2xl"> {location.includes("profile") ? <UserPlaceholder setUserData={setUserData} userData={userData} /> (
-
-) : ("Home")}</h2>
+					<h2 className="font-bold text-2xl">
+						{location.includes("profile") ? (
+							<UserPlaceholder setUserData={setUserData} userData={userData} />
+						) : (
+							"Home"
+						)}
+					</h2>
 					<StarBorderPurple500Icon />
 				</div>
 			</div>
